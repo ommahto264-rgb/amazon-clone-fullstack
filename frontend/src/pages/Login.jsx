6ef9ba798db631d6d91e9c6e52ec66c67297a445
ommahto264.rgb <ommahto264@gmail.com>
2026-06-30 02:27:25 +0530

    kjcnh

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
   
     
     try {
-      const response = await fetch('http://localhost:3000/api/auth/login', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
